008535bcdeedaac70a973fb4fd5419988291cdde
munyamugb <munyaradzi045@gmail.com>
2023-12-20 22:36:41 +0200

    Refactor code into saperate reusable funtions

--- index.js
+++ index.js
@@ -1,48 +1,62 @@
 'use-strict';
+
+// Selecting necessary DOM elements
 const userHeader = document.querySelector('.user-header');
 const textInput = document.querySelector('#header-text-input');
 const instructions = document.querySelector('.instruction-card');
 const userTextInput = document.querySelector('.user-header-input');
 
-textInput.addEventListener('input', (event) => {
+// Function to show or hide instructions based on input value
+const showInstructions = (inputValue) => {
+  instructions.style.display = (inputValue === '/' || inputValue === '/1') ? 'block' : 'none';
+}
+
+// Function to clear input field and hide its display
+const clearInput = (inputElement) => {
+  inputElement.style.display = 'none';
+  inputElement.value = '';
+}
+
+// Function triggered when '/' or '1' is pressed
+const handleTextInput = (event) => {
   const inputValue = event.target.value.trim();
 
-  if (inputValue === '/' || inputValue === '/1') {
-    instructions.style.display = 'block';
-  } else {
-    instructions.style.display = 'none';
-  }
-});
-
-textInput.addEventListener('keypress', (event) => {
-  const inputValue = event.target.value.trim();
-
+  // When 'Enter' key is pressed and input matches '/1'
   if (event.key === 'Enter' && inputValue === '/1') {
     userTextInput.style.display = 'block';
     instructions.style.display = 'none';
     textInput.blur();
-    textInput.style.display = 'none';
-
-    // Clear the text input value
-    textInput.value = '';
+    clearInput(textInput);
   }
-});
+}
 
+// Function triggered when 'Enter' is pressed in user input field
+const handleUserTextInput = (event) => {
+  const inputValue = event.target.value.trim();
+
+  // When 'Enter' key is pressed and input is not empty
+  if (event.key === 'Enter' && inputValue !== '') {
+    createHeader(inputValue);
+    clearInput(userTextInput);
+    userTextInput.style.display = 'none';
+    textInput.style.display = 'block'; 
+  } else {
+    console.log('error'); // Log an error if input is empty
+  }
+}
+
+// Function to create a header element with provided text
 const createHeader = (input) => {
   const h1Element = document.createElement('h1');
   h1Element.textContent = input;
   userHeader.appendChild(h1Element);
-};
+}
 
-userTextInput.addEventListener('keypress', (event) => {
-  const inputValue = event.target.value.trim();
-
-  if (event.key === 'Enter' && inputValue !== '') {
-    createHeader(inputValue);
-    userTextInput.style.display = 'none';
-    textInput.style.display = 'block';
-
-    // Clear the userTextInput value
-    userTextInput.value = '';
-  }
+// Event listeners for input and keypress events
+textInput.addEventListener('input', (event) => {
+  showInstructions(event.target.value.trim()); // Show/hide instructions based on input
 });
+
+textInput.addEventListener('keypress', handleTextInput); // Listen for keypress in main text input
+
+userTextInput.addEventListener('keypress', handleUserTextInput); // Listen for keypress in user input
